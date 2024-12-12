c4c3adb602ff4ffe224c0bacf24b3e251302674c
Sephia Laureencia <hemas.laureencia@sjsu.edu>
2024-12-12 13:25:02 +0700

    add Dropzone component for pdf upload

---
+++ src/app/components/Dropzone.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useMemo, CSSProperties, useState, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+
+// Type assertion to prevent "Type 'string' is not assignable to type ..." errors
+// https://github.com/frenic/csstype#what-should-i-do-when-i-get-type-errors
+const baseStyle: CSSProperties = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const focusedStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+export default function FileDropzone() {
+  const [previewUrl, setPreviewUrl] = useState<string>();
+  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
+    useDropzone({
+      accept: {
+        "application/pdf": [".pdf"],
+      },
+      maxFiles: 1,
+      onDrop: (acceptedFiles) => {
+        setPreviewUrl(URL.createObjectURL(acceptedFiles[0]));
+      },
+    });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    if (previewUrl === undefined) return;
+    return () => URL.revokeObjectURL(previewUrl);
+  }, [previewUrl]);
+
+  return (
+    <div>
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <p>drag & drop ur gojek wrapped file here</p>
+      </div>
+      {previewUrl !== undefined && (
+        <iframe src={previewUrl} className="w-full h-[50vh] pt-6" />
+      )}
+    </div>
+  );
+}
