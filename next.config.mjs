ec9c3acc28e957379050e671c09556a2d2a89781
Kevin Mingtarja <kevinm@hypermode.com>
2024-12-14 20:09:15 +0700

    add HSTS header

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // https://github.com/wojtekmaj/react-pdf?tab=readme-ov-file#nextjs
   swcMinify: false,
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     return config;
   },
+  headers: {
+    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
+  }
 };
 
 export default nextConfig;
